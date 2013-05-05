3c21b8b307c5490eca284776eba493f351b2be23
Zachary <zach@zachshuford.com>
2013-05-04 21:42:29 -0400

    Created Database

---
+++ Hackathon/SampleClass.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hackathon
+{
+    public class SampleClass
+    {
+        HTDataSet myDataSet;
+        HTDataSetTableAdapters.customer_dataTableAdapter myCustomerAdapter;
+
+        void MessWithData()
+        {
+            myDataSet = new HTDataSet();
+            myCustomerAdapter = new HTDataSetTableAdapters.customer_dataTableAdapter();
+            myCustomerAdapter.Fill(myDataSet.customer_data);
+
+            IEnumerable<HTDataSet.customer_dataRow> myEnumerableRows =
+                from row in myDataSet.customer_data
+                where row.net_sales > 100        
+                select row;
+
+
+            //If you have Head First C#, this syntax is thorougly explained on page 699.
+            //It is returning an enumerable squence of enumerbale sequence (so the first
+            //sequence is the lowest number segID with sales over 100. The next group is
+            //the next lowest SegID with sales over 100, etc.
+            var gropuedRows =
+                from row in myDataSet.customer_data
+                where row.net_sales > 100
+                group row by row.seg_id
+                    into segIdGroup
+                    orderby segIdGroup.Key
+                    select segIdGroup;
+
+        }
+    }
+}
