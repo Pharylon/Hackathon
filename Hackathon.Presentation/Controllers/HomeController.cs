917c58b4a10907b9048598ad2c825230dea9a1cf
Patrick Graham <pabreetzio@gmail.com>
2013-05-17 01:39:02 -0400

    Adding D3.js, example barchart on Index.cshtml

--- Hackathon.Presentation/Controllers/HomeController.cs
+++ Hackathon.Presentation/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -15,6 +16,19 @@ namespace Hackathon.Presentation.Controllers
         {
             return View();
         }
-
+        [HttpGet]
+        public JsonResult Data()
+        {
+            Element hydrogen = new Element() { Symbol = "H", AtomicMass = 1.07947 };
+            Element helium = new Element() { Symbol = "He", AtomicMass = 4.002602 };
+            Element lithium = new Element() { Symbol = "Li", AtomicMass = 6.941 };
+           List<Element> myElements = new List<Element>(){hydrogen, helium, lithium};
+            return Json(myElements, JsonRequestBehavior.AllowGet);
+        }
     }
+        public class Element
+        {
+            public string Symbol { get; set; }
+            public double AtomicMass { get; set; }
+        }
 }
