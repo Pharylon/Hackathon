0c9737779e85daf686f5b097225f7d28cf10cf9e
Patrick Graham <pabreetzio@gmail.com>
2013-05-18 12:12:27 -0400

    Renamed Hackathon Project to Hackathon.Domain

---
+++ Hackathon.Domain/NeighborhoodBorderMarker.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hackathon.Domain
+{
+    class NeighborhoodBorderMarker
+    {
+        public virtual int Id{ get ; set; }
+        public virtual int Latitude { get; set; }
+        public virtual int Longitude { get; set; }
+    }
+}
---
+++ Hackathon.Domain/Transaction.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hackathon.Domain
+{
+    public class Transaction
+    {
+        public virtual int Id { get; set; }
+        public virtual decimal sales { get; set; }
+        public virtual string description { get; set; }
+        public virtual int Household { get; set; }
+    }
+}
